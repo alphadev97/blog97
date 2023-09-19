8a7ea590ae4565bbfb87d9d580e2ad2e94e945de
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-19 18:32:24 +0500

    Home

--- app/page.jsx
+++ app/page.jsx
@@ -6,6 +6,12 @@ const Home = () => {
         <br className="max-md:hidden " />
         <span className="blue_gradient text-center">AI Powered Prompts</span>
       </h1>
+      <p className="desc text-center">
+        Prompty97 is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+
+      {/* Feed */}
     </section>
   );
 };
