2ee215d8b8365e39f4d10018d202259bc696495c
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-19 18:30:20 +0500

    Home

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,13 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Dicover & Share
+        <br className="max-md:hidden " />
+        <span className="blue_gradient text-center">AI Powered Prompts</span>
+      </h1>
+    </section>
+  );
 };
 
 export default Home;
