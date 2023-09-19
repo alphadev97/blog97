d2ce5a8ab6a12977f5f41aa7b6ee11449d0f684b
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-20 01:32:43 +0500

    layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,13 @@
+import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Blog97 App",
-  description: "The best blog app!",
+  title: "Blog97",
+  description: "A Full Stack Blog App with Next.js",
 };
 
 export default function RootLayout({ children }) {
@@ -13,7 +15,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div className="container">
-          <div className="wrapper">{children}</div>
+          <div className="wrapper">
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
         </div>
       </body>
     </html>
