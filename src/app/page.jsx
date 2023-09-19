80676f70a9168fbe4798ee2aeb27a4fa5a6f5e9d
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-20 01:42:15 +0500

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,19 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/menu/Menu";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList />
+        <Menu />
+      </div>
+    </div>
+  );
 }
