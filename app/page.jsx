23d12e59d14bc497f5b9709f0c7319dc48d905aa
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-19 18:36:10 +0500

    initial components

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
@@ -12,6 +14,7 @@ const Home = () => {
       </p>
 
       {/* Feed */}
+      <Feed />
     </section>
   );
 };
