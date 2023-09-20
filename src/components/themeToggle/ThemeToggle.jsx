e895ee676dc471bd92c0e1d424b395571bacad2a
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-20 17:33:32 +0500

    ThemeToggle

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
 import styles from "./themeToggle.module.css";
 
 const ThemeToggle = () => {
-  return <div className={styles.container}>ThemeToggle</div>;
+  return (
+    <div className={styles.container}>
+      <Image src={"/moon.png"} width={14} height={14} />
+      <div className={styles.ball}></div>
+      <Image src={"/sun.png"} width={14} height={14} />
+    </div>
+  );
 };
 
 export default ThemeToggle;
