60dc7098e0d360a954bd38a195756614e4c64bf1
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-20 18:31:13 +0500

    ThemeContext

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -5,8 +5,10 @@ import { createContext, useState } from "react";
 export const ThemeContext = createContext();
 
 const getFromLocalStorage = () => {
-  const value = localStorage.getItem("theme");
-  return value || "light";
+  if (typeof window !== undefined) {
+    const value = localStorage.getItem("theme");
+    return value || "light";
+  }
 };
 
 export const ThemeContextProvider = ({ children }) => {
@@ -14,5 +16,7 @@ export const ThemeContextProvider = ({ children }) => {
     return getFromLocalStorage();
   });
 
-  return <ThemeContext.Provider>{children}</ThemeContext.Provider>;
+  return (
+    <ThemeContext.Provider value={{ theme }}>{children}</ThemeContext.Provider>
+  );
 };
