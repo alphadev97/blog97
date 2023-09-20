6637b3fdb49b9c112da32259836aa10e211aa9b1
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-21 01:40:12 +0500

    FontUpdated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,11 @@
 import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import Footer from "@/components/footer/Footer";
 import { ThemeContextProvider } from "@/context/ThemeContext";
 import ThemeProvider from "@/providers/ThemeProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Blog97",
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={montserrat.className}>
         <ThemeContextProvider>
           <ThemeProvider>
             <div className="container">
