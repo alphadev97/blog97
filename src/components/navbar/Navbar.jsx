88726c4216fafa7a038e071f877c8dc36625da3a
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-20 17:25:21 +0500

    NavBar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "./navbar.module.css";
 import Image from "next/image";
+import Link from "next/link";
+import AuthLinks from "../authLinks/AuthLinks";
+import ThemeToggle from "../themeToggle/ThemeToggle";
 
 const Navbar = () => {
   return (
@@ -11,8 +14,14 @@ const Navbar = () => {
         <Image src={"/tiktok.png"} alt="tiktok" width={24} height={24} />
         <Image src={"/youtube.png"} alt="youtube" width={24} height={24} />
       </div>
-      <div className={styles.logo}></div>
-      <div className={styles.links}></div>
+      <div className={styles.logo}>Blog97</div>
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link href={"/"}>Homepage</Link>
+        <Link href={"/"}>Contact</Link>
+        <Link href={"/"}>About</Link>
+        <AuthLinks />
+      </div>
     </div>
   );
 };
