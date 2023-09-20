43e327c9e8d6a3ac036eb4105fbc6cd65fcff920
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-20 16:55:53 +0500

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import styles from "./navbar.module.css";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.social}></div>
+      <div className={styles.social}>
+        <Image src={"/facebook.png"} alt="Facebook" width={24} height={24} />
+        <Image src={"/instagram.png"} alt="Instagram" width={24} height={24} />
+        <Image src={"/tiktok.png"} alt="tiktok" width={24} height={24} />
+        <Image src={"/youtube.png"} alt="youtube" width={24} height={24} />
+      </div>
       <div className={styles.logo}></div>
       <div className={styles.links}></div>
     </div>
