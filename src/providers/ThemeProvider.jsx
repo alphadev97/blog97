8827470a7cc69466f1f21eb85e2520574668d313
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-20 18:31:21 +0500

    ThemeProvider

---
+++ src/providers/ThemeProvider.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { ThemeContext } from "@/context/ThemeContext";
+import React, { useContext } from "react";
+
+const ThemeProvider = ({ children }) => {
+  const { theme } = useContext(ThemeContext);
+
+  return <div className={theme}>{children}</div>;
+};
+
+export default ThemeProvider;
