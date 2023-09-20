ea00225b768551812c1290a2abe7d036f5dba9be
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-20 18:30:58 +0500

    ThemeToggle

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,7 +1,15 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./themeToggle.module.css";
+import { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
 
 const ThemeToggle = () => {
+  const { theme } = useContext(ThemeContext);
+
+  console.log(theme);
+
   return (
     <div className={styles.container}>
       <Image src={"/moon.png"} width={14} height={14} />
