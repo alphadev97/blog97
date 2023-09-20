80bd65e1335d8a50de70cb725935fd3dabe80bd8
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-20 16:50:15 +0500

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import styles from "./navbar.module.css";
 
 const Navbar = () => {
-  return <div className={styles.container}>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.social}></div>
+      <div className={styles.logo}></div>
+      <div className={styles.links}></div>
+    </div>
+  );
 };
 
 export default Navbar;
