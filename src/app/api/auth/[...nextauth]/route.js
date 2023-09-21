acb062188ceb660a42d5754a826c80628371b022
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-22 00:12:54 +0500

    NextAuth

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,4 @@
+import { authOptions } from "@/utils/auth";
+import NextAuth from "next-auth";
+
+export default NextAuth(authOptions);
--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import { Montserrat } from "next/font/google";
 import Footer from "@/components/footer/Footer";
 import { ThemeContextProvider } from "@/context/ThemeContext";
 import ThemeProvider from "@/providers/ThemeProvider";
+import AuthProvider from "@/providers/AuthProvider";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -16,17 +17,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={montserrat.className}>
-        <ThemeContextProvider>
-          <ThemeProvider>
-            <div className="container">
-              <div className="wrapper">
-                <Navbar />
-                {children}
-                <Footer />
+        <AuthProvider>
+          <ThemeContextProvider>
+            <ThemeProvider>
+              <div className="container">
+                <div className="wrapper">
+                  <Navbar />
+                  {children}
+                  <Footer />
+                </div>
               </div>
-            </div>
-          </ThemeProvider>
-        </ThemeContextProvider>
+            </ThemeProvider>
+          </ThemeContextProvider>
+        </AuthProvider>
       </body>
     </html>
   );
