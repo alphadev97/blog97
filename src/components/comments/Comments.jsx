d1072ce77ff5ed2b19831a9b2b464c5bc4722f1a
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-21 20:10:08 +0500

    Comments

---
+++ src/components/comments/Comments.jsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import styles from "./comments.module.css";
+import { Link } from "next/link";
+
+const Comments = () => {
+  const status = "authenticated";
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Comments</h1>
+      {status === "authenticated" ? (
+        <div className={styles.write}>
+          <textarea placeholder="write a comment" className={styles.input} />
+          <button className={styles.button}>Send</button>
+        </div>
+      ) : (
+        <Link href={"/login"}>Login to write a comment</Link>
+      )}
+
+      <div className={styles.comments}>
+        <div className={styles.comment}>
+          <div className={styles.user}>
+            <Image
+              src={"/p1.jpeg"}
+              alt=""
+              width={50}
+              height={50}
+              className={styles.image}
+            />
+            <div className={styles.userInfo}>
+              <span className={styles.username}>Mark John</span>
+              <span className={styles.date}>01.02.2023</span>
+            </div>
+          </div>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut
+            eligendi harum assumenda natus debitis dolores doloremque adipisci
+            distinctio odit labore neque veritatis iste nemo perferendis, vitae
+            dignissimos sequi quam voluptatem!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Comments;
