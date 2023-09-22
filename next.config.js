33f20ed7c064144604da62b7bcf2b37fa4924b58
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-23 00:42:28 +0500

    nextConfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+};
+
+module.exports = nextConfig;
