a0971dd5a79889d59fd2e9884ba61f3e61526701
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-25 20:44:15 +0500

    CardList

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 import styles from "./card.module.css";
 import Link from "next/link";
 
-const Card = () => {
+const Card = ({ key, item }) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} key={key}>
       <div className={styles.imageContainer}>
         <Image src={"/p1.jpeg"} alt="" fill className={styles.image} />
       </div>
@@ -14,7 +14,7 @@ const Card = () => {
           <span className={styles.category}>CULTURE</span>
         </div>
         <Link href={"/"}>
-          <h1>Lorem ipsum dolor sit amet consectetur adipiscing elit.</h1>
+          <h1>{item.title}</h1>
         </Link>
 
         <p className={styles.desc}>
