47dcc954692e0a446079c01223fb690d322c344a
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-25 16:23:05 +0500

    writePage

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -5,10 +5,23 @@ import styles from "./writePage.module.css";
 import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.bubble.css";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 const WritePage = () => {
+  const { status } = useSession();
+  const router = useRouter();
+
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
+
+  if (status === "loading") {
+    return <div className={styles.loading}>Loading...</div>;
+  }
+
+  if (status === "authenticated") {
+    router.push("/");
+  }
 
   return (
     <div className={styles.container}>
