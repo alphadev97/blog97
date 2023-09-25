7de725e42449d475d3406132359281c5ec35694b
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-25 21:09:37 +0500

    Pagination

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -4,19 +4,21 @@ import React from "react";
 import styles from "./pagination.module.css";
 import { useRouter } from "next/navigation";
 
-const Pagination = ({ page }) => {
+const Pagination = ({ page, hasPrev, hasNext }) => {
   const router = useRouter();
 
   return (
     <div className={styles.container}>
       <button
         className={styles.button}
+        disabled={!hasPrev}
         onClick={() => router.push(`?page=${page - 1}`)}
       >
         Previous
       </button>
       <button
         className={styles.button}
+        disabled={!hasNext}
         onClick={() => router.push(`?page=${page + 1}`)}
       >
         Next
