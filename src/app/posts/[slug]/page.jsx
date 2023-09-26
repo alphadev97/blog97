b55db161a59f30c728f3b546675bd58c307e38f0
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-26 18:25:47 +0500

    singlePost

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -26,46 +26,37 @@ const SinglePage = async ({ params }) => {
         <div className={styles.textContainer}>
           <h1 className={styles.title}>{data?.title}</h1>
           <div className={styles.user}>
-            {data?.img && (
+            {data?.user?.image && (
               <div className={styles.userImageContainer}>
-                <Image src={data.img} alt="" fill className={styles.avatar} />
+                <Image
+                  src={data.user.image}
+                  alt=""
+                  fill
+                  className={styles.avatar}
+                />
               </div>
             )}
             <div className={styles.userTextContainer}>
-              <span className={styles.username}>Mark John</span>
-              <span className={styles.date}>02.11.23</span>
+              <span className={styles.username}>{data?.user.name}</span>
+              <span className={styles.date}>
+                {data?.createdAt.substring(0, 10)}
+              </span>
             </div>
           </div>
         </div>
-        <div className={styles.imageContainer}>
-          <Image src={"/p1.jpeg"} alt="" fill className={styles.image} />
-        </div>
+        {data?.img && (
+          <div className={styles.imageContainer}>
+            <Image src={data.img} alt="" fill className={styles.image} />
+          </div>
+        )}
       </div>
       <div className={styles.content}>
         <div className={styles.post}>
-          <div className={styles.description}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipiscing elit varius
-              viverra, tristique id eget platea donec facilisi primis
-              suspendisse, fusce hac tellus molestie felis fringilla quis
-              faucibus.
-            </p>
-            <h2>
-              Lorem ipsum dolor sit amet consectetur adipiscing elit varius
-            </h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipiscing elit varius
-              viverra, tristique id eget platea donec facilisi primis
-              suspendisse, fusce hac tellus molestie felis fringilla quis
-              faucibus.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipiscing elit varius
-              viverra, tristique id eget platea donec facilisi primis
-              suspendisse, fusce hac tellus molestie felis fringilla quis
-              faucibus.
-            </p>
-          </div>
+          <div
+            className={styles.description}
+            dangerouslySetInnerHTML={{ __html: data?.desc }}
+          />
+
           <div className={styles.comment}>
             <Comments />
           </div>
