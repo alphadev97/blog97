03989564e79330393a3f3b906b98828bc40da10a
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-26 18:25:42 +0500

    singlePost

--- src/app/api/posts/[slug]/route.js
+++ src/app/api/posts/[slug]/route.js
@@ -8,6 +8,7 @@ export const GET = async (req, { params }) => {
   try {
     const post = await prisma.post.findUnique({
       where: { slug },
+      include: { user: true },
     });
 
     return new NextResponse(JSON.stringify(post, { status: 200 }));
