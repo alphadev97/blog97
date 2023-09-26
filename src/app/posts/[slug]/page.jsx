306e442edd6e067dbf9926dde48ecdc7e1c46291
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-26 17:42:58 +0500

    singlePost

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -3,18 +3,34 @@ import styles from "./singlePage.module.css";
 import Image from "next/image";
 import Comments from "@/components/comments/Comments";
 
-const SinglePage = () => {
+const getData = async (slug) => {
+  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed!");
+  }
+
+  return res.json();
+};
+
+const SinglePage = async ({ params }) => {
+  const { slug } = params;
+
+  const data = await getData(slug);
+
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>
-            Lorem ipsum dolor sit amet consectetur adipiscing elit,
-          </h1>
+          <h1 className={styles.title}>{data?.title}</h1>
           <div className={styles.user}>
-            <div className={styles.userImageContainer}>
-              <Image src={"/p1.jpeg"} alt="" fill className={styles.avatar} />
-            </div>
+            {data?.img && (
+              <div className={styles.userImageContainer}>
+                <Image src={data.img} alt="" fill className={styles.avatar} />
+              </div>
+            )}
             <div className={styles.userTextContainer}>
               <span className={styles.username}>Mark John</span>
               <span className={styles.date}>02.11.23</span>
