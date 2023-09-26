13a4864514b87f87f5c0793eb7b1c12264756433
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-26 18:25:37 +0500

    singlePost

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
 };
 
 module.exports = nextConfig;
