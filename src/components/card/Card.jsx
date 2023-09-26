8e12b51502f2c1376b24432ab683ea88e12d9721
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-26 17:30:08 +0500

    fetchPost

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,25 +5,24 @@ import Link from "next/link";
 const Card = ({ key, item }) => {
   return (
     <div className={styles.container} key={key}>
-      <div className={styles.imageContainer}>
-        <Image src={"/p1.jpeg"} alt="" fill className={styles.image} />
-      </div>
+      {item.img && (
+        <div className={styles.imageContainer}>
+          <Image src={item.img} alt="" fill className={styles.image} />
+        </div>
+      )}
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}>10.2.2022 - </span>
-          <span className={styles.category}>CULTURE</span>
+          <span className={styles.date}>
+            {item.createdAt.substring(0, 10)} -{" "}
+          </span>
+          <span className={styles.category}>{item.catSlug}</span>
         </div>
-        <Link href={"/"}>
+        <Link href={`/posts/${item.slug}`}>
           <h1>{item.title}</h1>
         </Link>
 
-        <p className={styles.desc}>
-          Lorem ipsum dolor sit amet consectetur adipiscing elit bibendum in
-          commodo, maecenas porttitor laoreet elementum tempus sem dis
-          parturient. Ultrices turpis quis auctor torquent justo facilisis
-          placerat ac duis...
-        </p>
-        <Link href={"/"} className={styles.link}>
+        <p className={styles.desc}>{item.desc.substring(0, 60)}</p>
+        <Link href={`/posts/${item.slug}`} className={styles.link}>
           Read More
         </Link>
       </div>
