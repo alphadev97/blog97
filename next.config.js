6d49cde144fbc1da88d23da361211c01e601d5eb
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-27 18:48:18 +0500

    createPost

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"],
   },
 };
 
