78c6dc5fb8a6a74aa5a1913fd66ae685f822f4e3
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-27 18:48:32 +0500

    createPost

--- src/app/api/posts/[slug]/route.js
+++ src/app/api/posts/[slug]/route.js
@@ -6,8 +6,9 @@ export const GET = async (req, { params }) => {
   const { slug } = params;
 
   try {
-    const post = await prisma.post.findUnique({
+    const post = await prisma.post.update({
       where: { slug },
+      data: { views: { increment: 1 } },
       include: { user: true },
     });
 
