39e8992afff13139c5e8b894912453aa8558cfd0
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-27 17:47:39 +0500

    createComment

--- src/app/api/comments/route.js
+++ src/app/api/comments/route.js
@@ -1,3 +1,4 @@
+import { getAuthSession } from "@/utils/auth";
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
@@ -15,6 +16,32 @@ export const GET = async (req) => {
     });
 
     return new NextResponse(JSON.stringify(comments, { status: 200 }));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
+  }
+};
+
+// CREATE A COMMENT
+export const POST = async (req) => {
+  const session = await getAuthSession();
+
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ message: "Not Authenticated!" }, { status: 401 })
+    );
+  }
+
+  try {
+    const body = await req.json();
+
+    const comment = await prisma.comment.create({
+      data: { ...body, userEmail: session.user.email },
+    });
+
+    return new NextResponse(JSON.stringify(comment, { status: 200 }));
   } catch (err) {
     console.log(err);
     return new NextResponse(
