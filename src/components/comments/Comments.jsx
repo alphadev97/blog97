4a25b330a5971cf358989edbcccacd926042019b
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-27 17:47:44 +0500

    createComment

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -5,6 +5,7 @@ import styles from "./comments.module.css";
 import { Link } from "next/link";
 import useSWR from "swr";
 import { useSession } from "next-auth/react";
+import { useState } from "react";
 
 const fetcher = async (url) => {
   const res = await fetch(url);
@@ -22,18 +23,34 @@ const fetcher = async (url) => {
 const Comments = ({ postSlug }) => {
   const { status } = useSession();
 
-  const { data, isLoading } = useSWR(
+  const { data, mutate, isLoading } = useSWR(
     `http://localhost:3000/api/comments?postSlug=${postSlug}`,
     fetcher
   );
+
+  const [desc, setDesc] = useState("");
+
+  const handleSubmit = async () => {
+    await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ desc, postSlug }),
+    });
+    mutate();
+  };
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Comments</h1>
       {status === "authenticated" ? (
         <div className={styles.write}>
-          <textarea placeholder="write a comment" className={styles.input} />
-          <button className={styles.button}>Send</button>
+          <textarea
+            placeholder="write a comment"
+            className={styles.input}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+          <button className={styles.button} onClick={handleSubmit}>
+            Send
+          </button>
         </div>
       ) : (
         <Link href={"/login"}>Login to write a comment</Link>
