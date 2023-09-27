3b6dd025630ef1a0e90fd25a7a5ea6d31046c930
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-27 17:56:42 +0500

    createPost

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -31,3 +31,29 @@ export const GET = async (req) => {
     );
   }
 };
+
+// CREATE A POST
+export const POST = async (req) => {
+  const session = await getAuthSession();
+
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ message: "Not Authenticated!" }, { status: 401 })
+    );
+  }
+
+  try {
+    const body = await req.json();
+
+    const post = await prisma.post.create({
+      data: { ...body, userEmail: session.user.email },
+    });
+
+    return new NextResponse(JSON.stringify(post, { status: 200 }));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
+  }
+};
